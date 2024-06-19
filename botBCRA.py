a71a603394e55f97c69a32e75ca00611bd059100
Diego Moreno Rico <113727255+diegomr949@users.noreply.github.com>
2024-06-19 18:04:21 -0300

    Update botBCRA.py

--- botBCRA.py
+++ botBCRA.py
@@ -3,61 +3,95 @@ import schedule
 import time
 from datetime import datetime, timedelta
 import requests
+import time
 
 # Claves API de Twitter
-api_key = '3yyJcDSnhUWS5RSFX0S3ACVss'
-api_secret = 'WzaecI0DwzIwAiB83VCbVPsCDcL1whFxllpiNYJHD0IOwcNGs3'
-bearer_token = 'AAAAAAAAAAAAAAAAAAAAAOFgtwEAAAAAk88AuVdooZJHnjY5v6oBaInL6w4%3DAnGWUbHhxD4khWWd1BOYCsI0YD39YzN6kgoZizb7DE3E1rysIq'
-access_token = '1791474455996182528-w3O9U1tGIFcTvRYV8tSQvUlbDJDhTd'
-access_token_secret = 'zJxHgJeFAZVzyPdKa2U03b9aUuUrXhrW8Vrt7EGMFXOzd'
+api_key = 'API_KEY'
+api_secret = 'API_SECRET'
+bearer_token = 'BEARER_TOKEN'
+access_token = 'ACCESS_TOKEN'
+access_token_secret = 'ACCESS_TOKEN_SECRET'
+
+# Autentificación en Twitter
+client = tweepy.Client(bearer_token, api_key, api_secret, access_token, access_token_secret)
 
 # Función para formatear la fecha
 def formatear_fecha(fecha):
     """
-    Formatea una fecha en formato YYYY-MM-DD.
+    Formatea una fecha en formato DD-MM-AAAA.
 
     Args:
         fecha (datetime): Objeto datetime de la fecha.
 
     Devuelve:
-        - Fecha formateada como string (YYYY-MM-DD).
+        str: Fecha formateada como string (DD-MM-AAAA).
     """
     return fecha.strftime("%Y-%m-%d")
 
+# Función para obtener datos de una variable específica en un rango de fechas
+def obtener_datos_variable(id_variable, fecha_desde, fecha_hasta):
+    """
+    Obtiene datos de una variable específica en un rango de fechas.
+
+    Args:
+        id_variable (int): Identificador de la variable.
+        fecha_desde (str): Fecha de inicio en formato YYYY-MM-DD.
+        fecha_hasta (str): Fecha de fin en formato YYYY-MM-DD.
+
+    Devuelve:
+        list: Lista de diccionarios con datos de la variable o None en caso de error.
+    """
+    url = f"https://api.bcra.gob.ar/estadisticas/v2.0/DatosVariable/{id_variable}/{fecha_desde}/{fecha_hasta}"
+    try:
+        respuesta = requests.get(url, verify=False)
+
+        if respuesta.status_code == 200:
+            contenido = respuesta.json().get("results", [])
+            return contenido
+        else:
+            print(f"Error al obtener datos de variable: {id_variable}, {respuesta.status_code}")
+            return None
+    except requests.exceptions.RequestException as error:
+        print(f"Error de conexión: {error}")
+        return None
+    
 # Función para publicar un tweet con los datos de la variable
-def publicar_tweet_variable(id_variable, descripcion, datos):
+def publicar_tweet_variable(client, id_variable, descripcion, datos):
     """
     Publica un tweet con la información de una variable y sus datos.
 
     Args:
+        client (tweepy.Client): Cliente de Tweepy.
         id_variable (int): Identificador de la variable.
         descripcion (str): Descripción de la variable.
         datos (list): Lista de diccionarios con datos de la variable.
     """
-    # Crea el tweet con la información formateada
+   # Crea el tweet con la información formateada
     hashtag = "#BCRAData"  # Puedes modificar el hashtag por defecto
-    tweet = f"{descripcion} ({id_variable})\n\n"
-    for dato in datos:
-        fecha = dato["fecha"]
-        valor = dato["valor"]
-        tweet += f"{fecha}: {valor}\n"
+    dato = datos[-1]  # Obtener solo el último dato de la lista
+    fecha = datetime.strptime(dato["fecha"], "%Y-%m-%d").strftime("%d-%m-%Y")  # Modificado el formato de fecha
+    valor = dato["valor"]
+    tweet = f"{descripcion} al {fecha}\n{valor}\n\n"
     tweet += f"#BCRA #EconomiaArgentina {hashtag}"
 
-    # Autentifica en Twitter
-    client = tweepy.Client(bearer_token=bearer_token, api_key=api_key, api_secret=api_secret, access_token=access_token, access_token_secret=access_token_secret)
+    # Imprimir el tweet en la consola
+    print(f"Tweet: {tweet}")
 
     # Publica el tweet
     try:
         client.create_tweet(text=tweet)
         print(f"Tweet publicado: {tweet}")
+    except tweepy.errors.TooManyRequests as e:
+        print("Demasiadas solicitudes. Esperando 15 minutos antes de intentar nuevamente...")
+        time.sleep(15 * 60)  # Espera 15 minutos antes de intentar nuevamente
+        publicar_tweet_variable(client, id_variable, descripcion, datos)  # Intenta publicar nuevamente
     except tweepy.TweepyException as e:
-        # Handle specific Tweepy exceptions
-        error_code = e.api_code
-        error_message = e.reason
-        print(f"Error al publicar tweet: {error_code} - {error_message}")
+        # Manejar otras excepciones de Tweepy
+        print(f"Error al publicar tweet: {e}")
     except Exception as e:
-        # Catch any other unexpected exceptions
+        # Capturar cualquier otra excepción inesperada
         print(f"Error general: {e}")
+
 
 # Definir las variables con su ID y descripción
 variables = [
@@ -97,49 +131,33 @@ variables = [
     {"id": 42, "descripcion": "Pases pasivos para el BCRA - Saldos (en millones de pesos)"}
 ]
 
-def obtener_datos_variable(id_variable, fecha_desde, fecha_hasta):
-    """
-    Obtiene datos de una variable específica en un rango de fechas.
+# Lista para realizar un seguimiento de las variables para las que ya se ha publicado un tweet
+variables_publicadas = []
 
-    Args:
-        id_variable (int): Identificador de la variable.
-        fecha_desde (str): Fecha de inicio en formato YYYY-MM-DD.
-        fecha_hasta (str): Fecha de fin en formato YYYY-MM-DD.
-
-    Devuelve:
-        - Lista de diccionarios con datos de la variable o None en caso de error.
-    """
-    url = f"https://api.bcra.gob.ar/{id_variable}/{fecha_desde}/{fecha_hasta}"  # Aquí deberías completar la URL base de la API
-    try:
-        respuesta = requests.get(url)
-        if respuesta.status_code == 200:
-            contenido = respuesta.json().get("results", [])
-            return contenido
-        else:
-            print(f"Error al obtener datos de variable: {id_variable}, {respuesta.status_code}")
-            return None
-    except requests.exceptions.RequestException as error:
-        print(f"Error de conexión: {error}")
-        return None
-
-# Función para twittear cada variable
 def twittear_variables():
+    """
+    Obtiene los datos de las variables y publica tweets para cada una.
+    """
     hoy = datetime.now()
-    fecha_desde = formatear_fecha(hoy - timedelta(days=1))
-    fecha_hasta = formatear_fecha(hoy)
-    
+    fecha_desde = formatear_fecha(hoy - timedelta(days=7))  # Cambia la fecha de inicio según sea necesario
+    fecha_hasta = formatear_fecha(hoy - timedelta(days=6))
     for variable in variables:
         id_variable = variable["id"]
         descripcion = variable["descripcion"]
+        # Verificar si ya se ha publicado un tweet para esta variable
+        if id_variable in variables_publicadas:
+            continue  # Saltar esta variable y pasar a la siguiente
         datos = obtener_datos_variable(id_variable, fecha_desde, fecha_hasta)
         if datos:
-            publicar_tweet_variable(id_variable, descripcion, datos)
+            publicar_tweet_variable(client, id_variable, descripcion, datos)  # Pasar el cliente como primer argumento
+            # Agregar esta variable a la lista de variables publicadas
+            variables_publicadas.append(id_variable)
 
 # Llamada directa para twittear las variables cuando se ejecute el script
 twittear_variables()
 
 # Programar la tarea diaria
-schedule.every().day.at("11:40").do(twittear_variables)
+#schedule.every().day.at("11:40").do(twittear_variables)
 
 # Ejecutar el scheduler en un bucle con manejo de excepciones
 while True:
@@ -150,3 +168,4 @@ while True:
         print(f"Error durante la ejecución del scheduler: {e}")
         # Podrías querer registrar el error o realizar otras acciones según sea necesario
         time.sleep(60)  # Espera antes de volver a intentar ejecutar el scheduler
+
